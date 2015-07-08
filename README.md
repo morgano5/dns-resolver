aac3794db451c2a91919d357206b8ed645853dde
Rafael Villar Villar <rafael@villar.me>
2015-07-08 15:12:34 +1000

    Adding some notes to README and some cleaning up

--- README.md
+++ README.md
@@ -1,17 +1,44 @@
 # DNS Resolver
 
-A Java DNS resolver.
+A non blocking DNS resolver for Java.
 
-This is still under construction. Basic functionality is already there but there are some issues to fix.
+This is a snippet of how to use this to get all type of RRs from a dns name (just while I write a more proper doc):
+
+```java
+
+    Resolver resolver = new Resolver();
+
+    Resolver.AnswerProcess process = resolver.lookup("yahoo.com", DnsType.ALL);
+
+    int timeout = 100;
+    boolean done;
+    do {
+
+        done = process.doIO(timeout); // It only blocks for 'timeout' milliseconds, returns true if there is an outcome
+
+        // do something else...
+
+    } while (!done);
+
+    List<ResourceRecord> result = process.getResult();
+
+    for(ResourceRecord rr: result) {
+        System.out.println("Result: " + rr.getDnsName() + " - " + rr.getDnsType() + " - " + rr.getData(Object.class));
+    }
+```
+
+This is still work in progress. Basic functionality is already there but there are some issues to fix.
 
 ## TODO list
 
-* Restart queries when a CNAME RR is reached
-* Support for other RRs specified in DNS updates and extensions
-* Support for MAILB, AXFR and other possible queries returning more than one type of RR
+- [ ] Write documentation
+- [ ] Restart queries when a CNAME RR is reached
+- [ ] Support for other RRs specified in DNS updates and extensions
+- [ ] Support for MAILB, AXFR and other possible queries returning more than one type of RR
+- [ ] Support for IPv6
 
 ## To consider
 
-* Support classes other than IN
+* Support classes other than __IN__
 * Support inverse queries
 
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,57 +2,26 @@ import au.id.villar.dns.DnsException;
 import au.id.villar.dns.Resolver;
 import au.id.villar.dns.engine.*;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Path rootDir = Paths.get("H:\\stuff\\03_code_and_projects\\03_java\\dns_code\\dns-resolver");
-		processNode(rootDir);
-	}
-
-	private static void processNode(Path node) {
-		try {
-			if (Files.isDirectory(node)) {
-				try (DirectoryStream<Path> stream = Files.newDirectoryStream(node)) {
-					for (Path child : stream) processNode(child);
-				}
-			} else {
-				try (InputStream stream = Files.newInputStream(node)) {
-					boolean previousWasCR = false;
-					int ch;
-					while((ch = stream.read()) != -1) {
-						if(previousWasCR && ch == 10) {
-							System.out.format("HIT: %s%n", node);
-							break;
-						}
-						previousWasCR = ch == 13;
-					}
-				}
-			}
-		} catch (IOException e) {
-			System.out.format("ERROR processing %s: %s%n", node, e.getMessage());
-		}
-	}
-
-
-	public static void main2(String[] args) {
 
 		Resolver resolver = new Resolver();
 
-		Resolver.AnswerProcess process = resolver.lookup("eluniversal.mx", DnsType.ALL);
+		Resolver.AnswerProcess process = resolver.lookup("villar.me", DnsType.ALL);
 
-			boolean done = process.doIO(100);
-			while (!done) {
-				done = process.doIO(100);
-			}
+		int timeout = 100;
+		boolean done;
+		do {
+
+			done = process.doIO(timeout);
+
+			// do something else...
+
+		} while (!done);
 
 		List<ResourceRecord> result = Collections.emptyList();
 		try {
